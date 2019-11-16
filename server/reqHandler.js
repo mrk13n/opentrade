a2bbe839024cf00250a6b53378956848e5de2a0d
Nazar <naz466@yandex.ru>
2019-11-16 13:44:11 +0200

    change path db

--- server/reqHandler.js
+++ server/reqHandler.js
@@ -16,6 +16,7 @@ const cors = require('cors');
 
 exports.handle = function(app, wss)
 {
+    console.log('1');
     app.get('/', onMain);
     app.get('/market/*', onMain);
     app.get('/index.html', onMain);
